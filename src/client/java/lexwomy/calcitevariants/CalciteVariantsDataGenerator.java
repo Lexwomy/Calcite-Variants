328076c65b77de0e030ad4c3b54f7c6271249a6a
Lexwomy <13265459+Lexwomy@users.noreply.github.com>
2026-05-10 00:10:24 -0500

    update gradle, loom, and adds optional vertical slabs if vertical slabs mod is installed

--- src/client/java/lexwomy/calcitevariants/CalciteVariantsDataGenerator.java
+++ src/client/java/lexwomy/calcitevariants/CalciteVariantsDataGenerator.java
@@ -1,6 +1,14 @@
 package lexwomy.calcitevariants;
 
+import static net.minecraft.data.BlockFamilies.familyBuilder;
+
 import com.mojang.datafixers.util.Pair;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import lexwomy.verticalslabs.VerticalSlabs;
+import lexwomy.verticalslabs.VerticalSlabsDataGenerator;
+import lexwomy.verticalslabs.data.BottomTopBasedBlockModelGenerator;
+import lexwomy.verticalslabs.data.VerticalSlabDetails;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
@@ -16,65 +24,97 @@ import net.minecraft.data.BlockFamily;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.storage.loot.LootTable;
-
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-
-import static net.minecraft.data.BlockFamilies.familyBuilder;
+import org.jspecify.annotations.Nullable;
 
 public class CalciteVariantsDataGenerator implements DataGeneratorEntrypoint {
-    private static final BlockFamily CALCITE = familyBuilder(Blocks.CALCITE)
-            .slab(CalciteVariants.CALCITE_SLAB)
-            .stairs(CalciteVariants.CALCITE_STAIRS)
-            .wall(CalciteVariants.CALCITE_WALL)
-            .getFamily();
+  private static final BlockFamily CALCITE =
+      familyBuilder(Blocks.CALCITE)
+          .slab(CalciteVariants.CALCITE_SLAB)
+          .stairs(CalciteVariants.CALCITE_STAIRS)
+          .wall(CalciteVariants.CALCITE_WALL)
+          .getFamily();
+  private static VerticalSlabDetails CALCITE_VERTICAL_SLAB_DETAILS;
 
-    private static class CalciteVariantsModelProvider extends FabricModelProvider {
-        private CalciteVariantsModelProvider(FabricDataOutput output) {
-            super(output);
-        }
-
-        @Override
-        public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
-            blockModelGenerators.family(CALCITE.getBaseBlock()).generateFor(CALCITE);
-        }
-
-        @Override
-        public void generateItemModels(ItemModelGenerators itemModelGenerators) {
-
-        }
+  @Override
+  public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    if (!CalciteVariants.hasVerticalSlabDependency
+        || CalciteVariants.CALCITE_VERTICAL_SLAB == null) {
+      throw new RuntimeException(
+          "Calcite Variants datagen needs to be run with vertical slabs dependency!");
     }
-    private static class CalciteVariantsLanguageProvider extends FabricLanguageProvider {
-        private static final List<Pair<String, String>> TRANSLATION_MAP = List.of(
+
+    CALCITE_VERTICAL_SLAB_DETAILS =
+        new VerticalSlabDetails(
+            "Calcite Vertical Slab",
+            CalciteVariants.CALCITE_VERTICAL_SLAB,
+            Blocks.CALCITE,
+            BottomTopBasedBlockModelGenerator.simpleUVLockedBlockModel(),
+            List.of(Blocks.CALCITE),
+            List.of(Blocks.CALCITE));
+    FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    pack.addProvider(CalciteVariantsModelProvider::new);
+    pack.addProvider(CalciteVariantsRecipeProvider::new);
+    pack.addProvider(CalciteVariantsLanguageProvider::new);
+    CalciteVariantsBlockTagProvider calciteVariantsBlockTagProvider =
+        pack.addProvider(CalciteVariantsBlockTagProvider::new);
+    pack.addProvider(
+        (output, registriesFuture) ->
+            new CalciteVariantsItemTagProvider(
+                output, registriesFuture, calciteVariantsBlockTagProvider));
+    pack.addProvider(CalciteVariantsBlockLootProvider::new);
+  }
+
+  private static class CalciteVariantsModelProvider extends FabricModelProvider {
+    private CalciteVariantsModelProvider(FabricDataOutput output) {
+      super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+      blockModelGenerators.family(CALCITE.getBaseBlock()).generateFor(CALCITE);
+      CALCITE_VERTICAL_SLAB_DETAILS.generateBlockModels(blockModelGenerators);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerators itemModelGenerators) {}
+  }
+
+  private static class CalciteVariantsLanguageProvider extends FabricLanguageProvider {
+    private static final List<Pair<String, String>> TRANSLATION_MAP =
+        List.of(
             new Pair<>("block.calcite_variants.calcite_slab", "Calcite Slab"),
             new Pair<>("block.calcite_variants.calcite_stairs", "Calcite Stairs"),
-            new Pair<>("block.calcite_variants.calcite_wall", "Calcite Wall")
-        );
-        private CalciteVariantsLanguageProvider(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
-            super(dataOutput, registryLookup);
-        }
+            new Pair<>("block.calcite_variants.calcite_wall", "Calcite Wall"));
 
-        @Override
-        public void generateTranslations(HolderLookup.Provider provider, TranslationBuilder translationBuilder) {
-            for (Pair<String, String> mapping : TRANSLATION_MAP) {
-                translationBuilder.add(mapping.getFirst(), mapping.getSecond());
-            }
-        }
+    private CalciteVariantsLanguageProvider(
+        FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
+      super(dataOutput, registryLookup);
     }
-    private static class CalciteVariantsRecipeProvider extends FabricRecipeProvider {
-        private CalciteVariantsRecipeProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-            super(output, registriesFuture);
-        }
 
-        @Override
-        protected RecipeProvider createRecipeProvider(HolderLookup.Provider provider, RecipeOutput recipeOutput) {
+    @Override
+    public void generateTranslations(
+        HolderLookup.Provider provider, TranslationBuilder translationBuilder) {
+      for (Pair<String, String> mapping : TRANSLATION_MAP) {
+        translationBuilder.add(mapping.getFirst(), mapping.getSecond());
+      }
+      CALCITE_VERTICAL_SLAB_DETAILS.generateTranslation(translationBuilder);
+    }
+  }
+
+  private static class CalciteVariantsRecipeProvider extends FabricRecipeProvider {
+    private CalciteVariantsRecipeProvider(
+        FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+      super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeProvider createRecipeProvider(
+        HolderLookup.Provider provider, RecipeOutput recipeOutput) {
       return new RecipeProvider(provider, recipeOutput) {
         @Override
         public void buildRecipes() {
@@ -85,44 +125,86 @@ public class CalciteVariantsDataGenerator implements DataGeneratorEntrypoint {
               RecipeCategory.BUILDING_BLOCKS, CalciteVariants.CALCITE_STAIRS, Blocks.CALCITE);
           this.stonecutterResultFromBase(
               RecipeCategory.BUILDING_BLOCKS, CalciteVariants.CALCITE_WALL, Blocks.CALCITE);
+          CALCITE_VERTICAL_SLAB_DETAILS.generateRecipes(this, recipeOutput);
         }
       };
-        }
-
-        @Override
-        public String getName() {
-            return "CalciteVariantsRecipeProvider";
-        }
     }
-    private static class CalciteVariantsBlockTagProvider extends FabricTagProvider.BlockTagProvider {
-        private CalciteVariantsBlockTagProvider(FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-            super(output, registriesFuture);
-        }
 
-        @Override
-        protected void addTags(HolderLookup.Provider wrapperLookup) {
-            valueLookupBuilder(BlockTags.MINEABLE_WITH_PICKAXE).add(CalciteVariants.CALCITE_SLAB, CalciteVariants.CALCITE_WALL, CalciteVariants.CALCITE_STAIRS).setReplace(false);
-        }
+    @Override
+    public String getName() {
+      return "Calcite Variants Recipe Provider";
     }
-    private static class CalciteVariantsBlockLootProvider extends FabricBlockLootTableProvider {
-        private CalciteVariantsBlockLootProvider(FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
-            super(dataOutput, registryLookup);
-        }
+  }
 
-        @Override
-        public void generate() {
-            this.add(CalciteVariants.CALCITE_SLAB, this::createSlabItemTable);
-            this.dropSelf(CalciteVariants.CALCITE_STAIRS);
-            this.dropSelf(CalciteVariants.CALCITE_WALL);
-        }
+  private static class CalciteVariantsBlockTagProvider extends FabricTagProvider.BlockTagProvider {
+    private CalciteVariantsBlockTagProvider(
+        FabricDataOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+      super(output, registriesFuture);
     }
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-        pack.addProvider(CalciteVariantsModelProvider::new);
-        pack.addProvider(CalciteVariantsRecipeProvider::new);
-        pack.addProvider(CalciteVariantsLanguageProvider::new);
-        pack.addProvider(CalciteVariantsBlockTagProvider::new);
-        pack.addProvider(CalciteVariantsBlockLootProvider::new);
-	}
+
+    @Override
+    protected void addTags(HolderLookup.Provider wrapperLookup) {
+      valueLookupBuilder(VerticalSlabs.VERTICAL_SLABS)
+          .add(CALCITE_VERTICAL_SLAB_DETAILS.slab())
+          .setReplace(false);
+      valueLookupBuilder(VerticalSlabs.VERTICAL_MINEABLE_SLABS)
+          .add(CALCITE_VERTICAL_SLAB_DETAILS.slab())
+          .setReplace(false);
+      valueLookupBuilder(BlockTags.MINEABLE_WITH_PICKAXE)
+          .add(
+              CalciteVariants.CALCITE_SLAB,
+              CalciteVariants.CALCITE_WALL,
+              CalciteVariants.CALCITE_STAIRS)
+          .setReplace(false);
+      valueLookupBuilder(BlockTags.SLABS).add(CalciteVariants.CALCITE_SLAB).setReplace(false);
+      valueLookupBuilder(BlockTags.STAIRS).add(CalciteVariants.CALCITE_STAIRS).setReplace(false);
+      valueLookupBuilder(BlockTags.WALLS).add(CalciteVariants.CALCITE_WALL).setReplace(false);
+    }
+  }
+
+  private static class CalciteVariantsItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    private CalciteVariantsItemTagProvider(
+        FabricDataOutput output,
+        CompletableFuture<HolderLookup.Provider> registriesFuture,
+        @Nullable BlockTagProvider blockTagProvider) {
+      super(output, registriesFuture, blockTagProvider);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider provider) {
+      valueLookupBuilder(VerticalSlabs.VERTICAL_SLABS_ITEMS)
+          .add(CALCITE_VERTICAL_SLAB_DETAILS.slab().asItem())
+          .setReplace(false);
+      valueLookupBuilder(VerticalSlabs.VERTICAL_MINEABLE_SLABS_ITEMS)
+          .add(CALCITE_VERTICAL_SLAB_DETAILS.slab().asItem())
+          .setReplace(false);
+      valueLookupBuilder(ItemTags.SLABS)
+          .add(CalciteVariants.CALCITE_SLAB.asItem())
+          .setReplace(false);
+      valueLookupBuilder(ItemTags.STAIRS)
+          .add(CalciteVariants.CALCITE_STAIRS.asItem())
+          .setReplace(false);
+      valueLookupBuilder(ItemTags.WALLS)
+          .add(CalciteVariants.CALCITE_WALL.asItem())
+          .setReplace(false);
+    }
+  }
+
+  private static class CalciteVariantsBlockLootProvider extends FabricBlockLootTableProvider {
+    private CalciteVariantsBlockLootProvider(
+        FabricDataOutput dataOutput, CompletableFuture<HolderLookup.Provider> registryLookup) {
+      super(dataOutput, registryLookup);
+    }
+
+    @Override
+    public void generate() {
+      this.add(CalciteVariants.CALCITE_SLAB, this::createSlabItemTable);
+      this.dropSelf(CalciteVariants.CALCITE_STAIRS);
+      this.dropSelf(CalciteVariants.CALCITE_WALL);
+      this.add(
+          CALCITE_VERTICAL_SLAB_DETAILS.slab(),
+          VerticalSlabsDataGenerator.VerticalSlabsBlockLootProvider.createVerticalSlabLootTable(
+              this, CALCITE_VERTICAL_SLAB_DETAILS.slab()));
+    }
+  }
 }
--- src/main/java/lexwomy/calcitevariants/CalciteVariants.java
+++ src/main/java/lexwomy/calcitevariants/CalciteVariants.java
@@ -1,8 +1,12 @@
 package lexwomy.calcitevariants;
 
+import java.util.function.Function;
+import lexwomy.verticalslabs.VerticalSlabs;
+import lexwomy.verticalslabs.block.VerticalSlab;
+import lexwomy.verticalslabs.block.VerticalSlabBlock;
 import net.fabricmc.api.ModInitializer;
-
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
@@ -13,53 +17,83 @@ import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.function.Function;
-
 public class CalciteVariants implements ModInitializer {
-	public static final String MOD_ID = "calcite_variants";
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  public static final String MOD_ID = "calcite_variants";
+  public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+  public static final Block CALCITE_SLAB =
+      register(
+          "calcite_slab", SlabBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE));
+  public static final Block CALCITE_STAIRS =
+      register(
+          "calcite_stairs",
+          (properties) -> new StairBlock(Blocks.CALCITE.defaultBlockState(), properties),
+          BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE));
+  public static final Block CALCITE_WALL =
+      register(
+          "calcite_wall", WallBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE));
+  public static boolean hasVerticalSlabDependency;
+  @Nullable public static Block CALCITE_VERTICAL_SLAB = null;
 
-    private static Block register(String name, Function<BlockBehaviour.Properties, Block> blockFactory, BlockBehaviour.Properties settings, boolean shouldRegisterItem) {
-        ResourceKey<Block> blockKey = keyOfBlock(name);
-        Block block = blockFactory.apply(settings.setId(blockKey));
+  private static Block register(
+      String name,
+      Function<BlockBehaviour.Properties, Block> blockFactory,
+      BlockBehaviour.Properties settings) {
+    ResourceKey<Block> blockKey = keyOfBlock(name);
+    Block block = blockFactory.apply(settings.setId(blockKey));
 
-        if (shouldRegisterItem) {
-            ResourceKey<Item> itemKey = keyOfItem(name);
+    ResourceKey<Item> itemKey = keyOfItem(name);
 
-            BlockItem blockItem = new BlockItem(block, new Item.Properties().setId(itemKey).useBlockDescriptionPrefix());
-            Registry.register(BuiltInRegistries.ITEM, itemKey, blockItem);
-        }
+    BlockItem blockItem =
+        new BlockItem(block, new Item.Properties().setId(itemKey).useBlockDescriptionPrefix());
+    Registry.register(BuiltInRegistries.ITEM, itemKey, blockItem);
 
-        return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
+    return Registry.register(BuiltInRegistries.BLOCK, blockKey, block);
+  }
+
+  private static ResourceKey<Block> keyOfBlock(String name) {
+    return ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(MOD_ID, name));
+  }
+
+  private static ResourceKey<Item> keyOfItem(String name) {
+    return ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, name));
+  }
+
+  @Override
+  public void onInitialize() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
+    hasVerticalSlabDependency = FabricLoader.getInstance().isModLoaded(VerticalSlabs.MOD_ID);
+    if (hasVerticalSlabDependency) {
+      CALCITE_VERTICAL_SLAB =
+          register(
+              "calcite_vertical_slab",
+              VerticalSlabBlock::new,
+              BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE));
+    } else {
+      LOGGER.info("Vertical slab mod dependency not found, skipping calcite vertical slabs");
     }
 
-    private static ResourceKey<Block> keyOfBlock(String name) {
-        return ResourceKey.create(Registries.BLOCK, Identifier.fromNamespaceAndPath(MOD_ID, name));
+    ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS)
+        .register(
+            (itemGroup) -> {
+              itemGroup.accept(CALCITE_SLAB.asItem());
+              itemGroup.accept(CALCITE_STAIRS.asItem());
+              itemGroup.accept(CALCITE_WALL.asItem());
+              if (CALCITE_VERTICAL_SLAB != null) {
+                itemGroup.accept(CALCITE_VERTICAL_SLAB.asItem());
+              }
+            });
+    if (CALCITE_VERTICAL_SLAB != null) {
+      ItemGroupEvents.modifyEntriesEvent(VerticalSlab.VERTICAL_SLAB_GROUP_KEY)
+          .register(
+              (itemGroup) -> {
+                itemGroup.accept(CALCITE_VERTICAL_SLAB);
+              });
     }
-
-    private static ResourceKey<Item> keyOfItem(String name) {
-        return ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(MOD_ID, name));
-    }
-
-    public static final Block CALCITE_SLAB = register("calcite_slab", SlabBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE), true);
-    public static final Block CALCITE_STAIRS = register("calcite_stairs", (properties) -> new StairBlock(Blocks.CALCITE.defaultBlockState(), properties), BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE), true);
-    public static final Block CALCITE_WALL = register("calcite_wall", WallBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.CALCITE), true);
-
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-
-        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register((itemGroup) -> {
-            itemGroup.accept(CALCITE_SLAB.asItem());
-            itemGroup.accept(CALCITE_STAIRS.asItem());
-            itemGroup.accept(CALCITE_WALL.asItem());
-        });
-	}
+  }
 }
